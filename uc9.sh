c435a97071d545c572b339455c7d57f3775fd493
Hemanth <kukunurihemanthsaikumar98@gmail.com>
2021-12-23 19:23:17 +0530

    added uc9.sh

---
+++ uc9.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -x
+
+isPartTime=1
+isFullTime=2
+maxHrsInMonth=100
+empRatePerHr=20
+numOfWorkingDays=20
+
+totalEmpHr=0
+totalWorkingDays=0
+
+declare -A dailyWages;
+function getWorkingHours()
+{
+        case $1 in
+                                $isFullTime)
+                                        empHrs=8
+                                ;;
+                                $isPartTime)
+                                        empHrs=4
+                                ;;
+                                *)
+                                        empHrs=0
+        esac
+}
+function getEmpWage()
+{
+    echo $(($1*$empRatePerHr))
+}
+while [[ $totalEmpHr -lt $maxHrsInMonth &&
+    $totalWorkingDays -lt $numOfWorkingDays ]]
+do
+   ((totalWorkingDays++))
+    randomcheck=$((RANDOM%3))
+    getWorkingHours $randomcheck
+     totalEmpHr=$(($totalEmpHr+$empHrs))
+      dailyWages["$totalWorkingDays"]=$(($empHrs*$empRatePerHr))
+done
+totalsalary="$( getEmpWage $totalEmpHr )"
+echo ${!dailyWages[@]}
